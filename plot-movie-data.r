b592867c7b0e01729c1fc7c8c479f90304cff990
aaronwolen <aaron@wolen.com>
2013-03-18 14:29:35 -0400

    Fixed stupid typo

--- plot-movie-data.r
+++ plot-movie-data.r
@@ -6,6 +6,6 @@ movies <- read.csv("movie-data.csv")
 
 
 # Generate and explort scatterplot
-s.plot <- qplot(budet, rating, data = movies)
+s.plot <- qplot(budget, rating, data = movies)
 
 ggsave(filename = "movie-scatterplot.png", plot = s.plot, width = 4, height = 4)
