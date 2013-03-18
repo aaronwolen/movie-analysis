ac7fc0cb96e1ae13a4e0f698978b7e18944c01e5
aaronwolen <aaron@wolen.com>
2013-03-18 14:42:20 -0400

    Increase point size fixes #1

--- plot-movie-data.r
+++ plot-movie-data.r
@@ -6,6 +6,6 @@ movies <- read.csv("movie-data.csv")
 
 
 # Generate and explort scatterplot
-s.plot <- qplot(budget, rating, data = movies) + stat_smooth(method = "lm")
+s.plot <- qplot(budget, rating, data = movies, size = I(5)) + stat_smooth(method = "lm")
 
 ggsave(filename = "movie-scatterplot.png", plot = s.plot, width = 4, height = 4)
